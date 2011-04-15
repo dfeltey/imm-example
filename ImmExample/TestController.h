b60723bb8315d8e1b256064f55dc8eb9565b793a
Sam Quinan <samquinan@uchicago.edu>
2011-04-15 00:46:16 -0500

    hey, it works! sort of...

---
+++ ImmExample/TestController.h
@@ -0,0 +1,16 @@
+//
+//  TestController.h
+//  ImmExample
+//
+//  Created by Sam Quinan on 4/14/11.
+//  Copyright 2011 University of Chicago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TestController : NSObject {
+    
+}
+
+@end
---
+++ ImmExample/TestController.m
@@ -0,0 +1,14 @@
+//
+//  TestController.m
+//  ImmExample
+//
+//  Created by Sam Quinan on 4/14/11.
+//  Copyright 2011 University of Chicago. All rights reserved.
+//
+
+#import "TestController.h"
+
+
+@implementation TestController
+
+@end
