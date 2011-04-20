aba4d7b30f70ba3103cb86f8256009059d5a6127
Sam Quinan <samquinan@uchicago.edu>
2011-04-19 20:16:11 -0500

    hangmanview working

---
+++ ImmExample/HangmanViewController.h
@@ -0,0 +1,16 @@
+//
+//  HangmanViewController.h
+//  ImmExample
+//
+//  Created by Sam Quinan on 4/18/11.
+//  Copyright 2011 University of Chicago. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HangmanViewController : UIViewController {
+    
+}
+
+@end
---
+++ ImmExample/HangmanViewController.m
@@ -0,0 +1,14 @@
+//
+//  HangmanViewController.m
+//  ImmExample
+//
+//  Created by Sam Quinan on 4/18/11.
+//  Copyright 2011 University of Chicago. All rights reserved.
+//
+
+#import "HangmanViewController.h"
+
+
+@implementation HangmanViewController
+
+@end
